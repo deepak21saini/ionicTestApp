7c25e2943cc098e1317fba3fac61bc125644f1f6
gurpreet <gurpreet@nascenture.com>
2018-09-11 12:07:23 +0530

    change

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -2,7 +2,7 @@ export class Config {
     public static API_ENDPOINT = "http://asm.nascenture.com/api/";
     public static SERVER_URL = "http://localhost:5000/";
 	public static BUSINESS_IMAGES = "https://cryptic-beach-64691.herokuapp.com/upload/";
-	public static VERIFICATION_TYPE = "mobile";
+	public static VERIFICATION_TYPE = "email";
 	public static API_URLS = {
 		LOGIN:   	 Config.API_ENDPOINT+"login",
 		REGISTER:    Config.API_ENDPOINT+"signup",
